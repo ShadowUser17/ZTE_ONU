cd48e38e977038352063ab2387e9d0c8fbf55153
ShadowUser17 <linuxforce17@gmail.com>
2020-09-12 15:13:24 +0300

    Code refactoring...

--- parsecfg/ztecfg.py
+++ parsecfg/ztecfg.py
@@ -3,88 +3,106 @@ from traceback import print_exc
 from collections import OrderedDict
 from re import compile as re_compile
 from sys import stdout
-#
-#
+
+
 class ParseZTE:
     _parser_olt = re_compile('interface gpon-olt_(\d+/\d+/\d+)')
     _parser_onu = re_compile('interface gpon-onu_(\d+/\d+/\d+:\d+)')
     _parser_mng = re_compile('pon-onu-mng gpon-onu_(\d+/\d+/\d+:\d+)')
-    #
+
+
     def __init__(self):
         self._raw = ''
         self._olt = ''
         self._onu = ''
         self._mng = ''
-    #
+
+
     def _loader(self, raw):
         raw = map(str.lstrip, raw.split('!'))
         self._raw = list(map(str.rstrip, raw))
-    #
+
+
     def _parser(self, regex):
         raw = iter(self._raw)
         data = OrderedDict()
-        #
+
         for item in raw:
             iface = regex.findall(item)
-            #
+
             if iface:
                 item = map(str.split, item.split('\n'))
                 item = list(map(list, item))
                 data[iface[0]] = item
-        #
+
         return data
-    #
+
+
     def parse_cfg(self):
         self._olt = self._parser(self._parser_olt)
         self._onu = self._parser(self._parser_onu)
         self._mng = self._parser(self._parser_mng)
         self._raw = ''
-    #
-    def load_from_string(self, raw): self._loader(raw)
-    #
+
+
+    def load_from_string(self, raw):
+        self._loader(raw)
+
+
     def load_from_file(self, fname):
-        with open(fname) as fd: self._loader(fd.read())
-    #
+        with open(fname) as fd:
+            self._loader(fd.read())
+
+
     def find_from_sn(self, sn):
         sn = sn.upper()
         onus = []
-        #
+
         for olt in self._olt.keys():
             onu = list(filter(lambda item: item[-1] == sn, self._olt[olt]))
-            if onu: onus.append((olt, onu[0]))
-        #
+            if onu:
+                onus.append((olt, onu[0]))
+
         return onus
-    #
+
+
     def get_by_sn(self, sn):
         onus = self.find_from_sn(sn)
-        #
+
         items = []
         for (olt, onu) in onus:
             olt_port = olt
             onu_port = '{}:{}'.format(olt_port, onu[1])
-            #
+
             olt = [self._olt[olt][0], onu]
             onu = self._onu[onu_port]
             mng = self._mng[onu_port]
-            #
+
             items.append((olt, onu, mng))
-        #
+
         return items
-    #
+
+
     #@classmethod
     #def replace_port(self, old, new, obj): pass
-    #
+
+
     @classmethod
     def print(self, obj, fd=stdout):
         obj = '\n'.join(map(' '.join, obj))
         print(obj + '\n!', file=fd)
-    #
-    @property
-    def cfg_olt(self): return self._olt
-    #
-    @property
-    def cfg_onu(self): return self._onu
-    #
-    @property
-    def cfg_mng(self): return self._mng
 
+
+    @property
+    def cfg_olt(self):
+        return self._olt
+
+
+    @property
+    def cfg_onu(self):
+        return self._onu
+
+
+    @property
+    def cfg_mng(self):
+        return self._mng
--- regonu/reg_onu_v2.py
+++ regonu/reg_onu_v2.py
@@ -3,34 +3,42 @@
 from traceback import print_exc
 from jinja2 import Template
 from argparse import ArgumentParser
-#
-#
+
+
 onu_types = {'1':'1ETH', '4':'4ETH'}
 args = ArgumentParser()
-#
+
 args.add_argument('onu_iface', help='Example: 1/2/5:1')
 args.add_argument('onu_sn', help='Example: ZTEGXXXXXXXX')
+
 args.add_argument('-t', dest='onu_type', default='1', help='ONU type: 1/4')
 args.add_argument('-m', dest='onu_mode', default='a', help='ONU type: a/h/t')
 args.add_argument('-d', dest='onu_desc', default='-', help='ONU description.')
 args.add_argument('-p', dest='onu_pvid', default='', help='Pvid for access/hybrid mode.')
 args.add_argument('-v', dest='onu_tags', default='', help='Vlans for hybrid/trunk mode.')
-#
-#
+
+
 try:
     args = args.parse_args()
     (olt_iface, onu_id) = args.onu_iface.split(':')
-    #
+
     onu_vlans = None
-    if args.onu_mode == 'a': onu_vlans = args.onu_pvid
-    elif args.onu_mode == 't': onu_vlans = args.onu_tags
+    if args.onu_mode == 'a':
+        onu_vlans = args.onu_pvid
+
+    elif args.onu_mode == 't':
+        onu_vlans = args.onu_tags
+
     elif args.onu_mode == 'h':
-        #
-        if not args.onu_tags: onu_vlans = args.onu_pvid
-        elif args.onu_pvid: onu_vlans = (args.onu_pvid + ',' + args.onu_tags) 
-    #
+        if not args.onu_tags:
+            onu_vlans = args.onu_pvid
+
+        elif args.onu_pvid:
+            onu_vlans = (args.onu_pvid + ',' + args.onu_tags) 
+
     with open('zte_onu.jn2') as fd:
         template = Template(fd.read())
+
         print(template.render(
             olt_iface=olt_iface,
             onu_id=onu_id,
@@ -43,5 +51,6 @@ try:
             onu_vlans=onu_vlans,
             onu_tags=args.onu_tags
         ))
-except Exception: print_exc()
 
+except Exception:
+    print_exc()
